ad5fae4100ebea5442ac61ee92756656393d05a1
fuldae9m <eike.fulda@ruhr-uni-bochum.de>
2020-07-13 17:46:01 +0200

    Sample config file

---
+++ config.sample.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host'      => 'localhost',
+    'rpcPort'   => '5864',
+    'rpcUser'   => 'user',
+    'rpcPass'   => 'password',
+    'proto'     => 'http',
+    'urlPath'   => null
+];
