88fb09595adb86bd92e8360d1822f38fd813fd32
fuldae9m <eike.fulda@ruhr-uni-bochum.de>
2020-08-10 20:03:29 +0200

    move the JS, to load and display updated data, to own file

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="icon" type="image/png" href="images/Gridcoin_32x32.png">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
   </head>
   <body>
     <header>
@@ -64,59 +65,6 @@
               <b>With client version:</b><br><div id='clientVersion'>" . $nodeStatus['showblock']['ClientVersion'] . "</div><br>
               </div></div></div>";
           ?>
-        <script>
-            setInterval(getFreshData, 30000);
-
-            function getFreshData()
-            {
-                $.getJSON("nodeStatus.php", updateData);
-            }
-            
-            function updateData(data)
-            {
-                if(!(data.getnetworkinfo === undefined))
-                {
-                    document.getElementById('nodeVersion').innerText = "Version: " + data.getnetworkinfo.version;
-
-                    let newConnectionCount = data.getnetworkinfo.connections;
-                    let conEl = document.getElementById("connectionCount");
-                    if(newConnectionCount > 8)
-                    {
-                        conEl.className = "content green";
-                    } else if( newConnectionCount > 0)
-                    {
-                        conEl.className = "content orange";
-                    } else
-                    {
-                        conEl.className = "content red";
-                    }
-                    conEl.innerText = newConnectionCount;
-                }
-
-                if(!(data.getwalletinfo === undefined))
-                {
-                    let walletLock = document.getElementById('walletLock');
-                    if(data.getwalletinfo.staking)
-                    {
-                        walletLock.innerText = "Staking";
-                        walletLock.className = "content green";
-                    } else
-                    {
-                        walletLock.innerText = "Wallet locked";
-                        walletLock.className = "conetnt red";
-                    }
-                }
-
-                if(!(data.showblock === undefined))
-                {
-                    document.getElementById('blockCount').innerText = data.showblock.height;
-                    document.getElementById('difficulty').innerText = data.showblock.difficulty;
-                    document.getElementById('hash').innerText = data.showblock.hash;
-                    document.getElementById('CPID').innerText = data.showblock.CPID;
-                    document.getElementById('clientVersion').innerText = data.showblock.ClientVersion;
-                }
-            }
-        </script>
     </main>
     <footer>
       <!--<img alt="Gridcoin Logo" src="https://raw.githubusercontent.com/gridcoin-community/Gridcoin-Marketing/master/Gridcoin%20Logos/PNG%20Format/Horizontal/GRCHorizontal_Purple_Transparent.png">-->
---
+++ script.js
@@ -0,0 +1,54 @@
+window.onload = function setUpdateInterval()
+{
+    setInterval(getFreshData, 30000);
+}
+
+function getFreshData()
+{
+    $.getJSON("nodeStatus.php", updateData);
+}
+
+function updateData(data)
+{
+    if(!(data.getnetworkinfo === undefined))
+    {
+        document.getElementById('nodeVersion').innerText = "Version: " + data.getnetworkinfo.version;
+
+        let newConnectionCount = data.getnetworkinfo.connections;
+        let conEl = document.getElementById("connectionCount");
+        if(newConnectionCount > 8)
+        {
+            conEl.className = "content green";
+        } else if( newConnectionCount > 0)
+        {
+            conEl.className = "content orange";
+        } else
+        {
+            conEl.className = "content red";
+        }
+        conEl.innerText = newConnectionCount;
+    }
+
+    if(!(data.getwalletinfo === undefined))
+    {
+        let walletLock = document.getElementById('walletLock');
+        if(data.getwalletinfo.staking)
+        {
+            walletLock.innerText = "Staking";
+            walletLock.className = "content green";
+        } else
+        {
+            walletLock.innerText = "Wallet locked";
+            walletLock.className = "conetnt red";
+        }
+    }
+
+    if(!(data.showblock === undefined))
+    {
+        document.getElementById('blockCount').innerText = data.showblock.height;
+        document.getElementById('difficulty').innerText = data.showblock.difficulty;
+        document.getElementById('hash').innerText = data.showblock.hash;
+        document.getElementById('CPID').innerText = data.showblock.CPID;
+        document.getElementById('clientVersion').innerText = data.showblock.ClientVersion;
+    }
+}
