41c6a93ab428511c2258a7272d207a3402c86182
LifeEncrypter <e50389.fulda@gmx.de>
2020-11-24 00:15:33 +0100

    Remove jQuery from /block/index

--- block/index.php
+++ block/index.php
@@ -9,8 +9,6 @@
     echo "\t<title>" . $config['siteName'] . "</title>\n";?>
     <link rel="stylesheet" type="text/css" href="../style.css">
     <link rel="icon" type="image/png" href="../images/Gridcoin_32x32.png">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="../script.js"></script>
   </head>
   <body>
     <header>
