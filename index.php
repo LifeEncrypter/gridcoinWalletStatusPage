8dcd833c21b176c15da4b146d1f76ad7c8419918
LifeEncrypter <e50389.fulda@gmx.de>
2021-10-06 14:40:17 +0200

    coding cosmetics

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Eike Fulda">
     <?php $config = require_once('config.php');
-    echo "<meta name=\"Description\" content=\"" . $config['siteDescription'] . "\">\n";
-    echo "\t<title>" . $config['siteName'] . "</title>\n";?>
+    echo "<meta name=\"Description\" content=\"" . $config['siteDescription'] . "\">\n";?>
+    <title><?php echo $config['siteName'];?></title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="icon" type="image/png" href="images/Gridcoin_32x32.png">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
